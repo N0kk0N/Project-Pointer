df18c72e664b78841e1dc49602bf0aa08a7c46b4
N0kk0N <jessekaptein18@gmail.com>
2025-01-07 21:00:03 +0100

    Add footer component and reorganize imports in the main page

---
+++ my-app/src/components/footer.svelte
@@ -0,0 +1,27 @@
+<div class="bg-[#f7f2ee]">
+    <div class="flex">
+        <div class="bg-blue-500">
+            <div>
+                <h2>
+                    Ons onderzoek<br/>
+                    begint bij jou
+                </h2>
+            </div>
+            <div>
+                <!-- HIER KOMEN ALLE SOCIALS -->
+            </div>
+        </div>
+
+        <div class="bg-red-500">
+            <p>Vak 2</p>
+        </div>
+
+        <div class="bg-pink-500">
+            <p>Vak 3</p>
+        </div>
+    </div>
+
+    <div>
+        <!-- HIER KOMT HET KRO LOGO -->
+    </div>
+</div>
