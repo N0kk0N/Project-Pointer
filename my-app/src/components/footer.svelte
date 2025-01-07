a06575a70374064d1997d6c0e6ea1c356f60f7e3
N0kk0N <jessekaptein18@gmail.com>
2025-01-07 21:47:48 +0100

    Enhance footer with social media links and improve layout in header

--- my-app/src/components/footer.svelte
+++ my-app/src/components/footer.svelte
@@ -1,14 +1,123 @@
-<div class="bg-[#f7f2ee]">
+<div class="bg-[#f7f2ee] h-80">
     <div class="flex">
-        <div class="bg-blue-500">
+        <div class="bg-blue-500 pt-10 pl-28">
             <div>
-                <h2>
-                    Ons onderzoek<br/>
+                <h2 class="text-6xl">
+                    Ons onderzoek<br />
                     begint bij jou
                 </h2>
             </div>
-            <div>
-                <!-- HIER KOMEN ALLE SOCIALS -->
+            <div class="flex items-center gap-5 pt-10">
+                <a
+                    href="#/"
+                    target="_blank"
+                    aria-label="Link naar onze Facebook."
+                >
+                    <svg
+                        viewBox="-0.5 -0.5 16 16"
+                        fill="black"
+                        xmlns="http://www.w3.org/2000/svg"
+                        id="Facebook--Streamline-Iconoir"
+                        height="24"
+                        width="24"
+                    >
+                        <path
+                            d="M11.2604375 0.6628125h-2.051125c-0.9066875000000001 0 -1.77625 0.36012500000000003 -2.4173125 1.00125 -0.6411250000000001 0.6411250000000001 -1.0013125 1.5106875 -1.0013125 2.4173125v2.0511874999999997H3.7395625v2.7348749999999997h2.051125v5.4697499999999994h2.7348749999999997v-5.4697499999999994h2.0511874999999997l0.6836875 -2.7348749999999997h-2.7348749999999997V4.0813749999999995c0 -0.18131250000000002 0.0720625 -0.35525 0.20025 -0.48343749999999996 0.12825 -0.12825 0.3021875 -0.20025 0.4835 -0.20025h2.051125V0.6628125Z"
+                            stroke="#000000"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            stroke-width="1"
+                        ></path>
+                    </svg>
+                </a>
+
+                <a
+                    href="#/"
+                    target="_blank"
+                    aria-label="Link naar onze Instagram."
+                >
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="24"
+                        height="24"
+                        viewBox="0 0 24 24"
+                        fill="currentColor"
+                        class="icon icon-tabler icons-tabler-filled icon-tabler-brand-instagram"
+                    >
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                        <path
+                            d="M16 3a5 5 0 0 1 5 5v8a5 5 0 0 1 -5 5h-8a5 5 0 0 1 -5 -5v-8a5 5 0 0 1 5 -5zm-4 5a4 4 0 0 0 -3.995 3.8l-.005 .2a4 4 0 1 0 4 -4m4.5 -1.5a1 1 0 0 0 -.993 .883l-.007 .127a1 1 0 0 0 1.993 .117l.007 -.127a1 1 0 0 0 -1 -1"
+                        />
+                    </svg>
+                </a>
+
+                <a
+                    href="#/"
+                    target="_blank"
+                    aria-label="Link naar onze Threads."
+                    class="bg-black"
+                >
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="24"
+                        height="24"
+                        viewBox="0 0 24 24"
+                        fill="black"
+                        stroke="white"
+                        stroke-width="2"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        class="icon icon-tabler icons-tabler-outline icon-tabler-brand-threads"
+                    >
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                        <path
+                            d="M19 7.5c-1.333 -3 -3.667 -4.5 -7 -4.5c-5 0 -8 2.5 -8 9s3.5 9 8 9s7 -3 7 -5s-1 -5 -7 -5c-2.5 0 -3 1.25 -3 2.5c0 1.5 1 2.5 2.5 2.5c2.5 0 3.5 -1.5 3.5 -5s-2 -4 -3 -4s-1.833 .333 -2.5 1"
+                        />
+                    </svg>
+                </a>
+
+                <a
+                    href="#/"
+                    target="_blank"
+                    aria-label="Link naar onze LinkedIn."
+                >
+                    <svg
+                        role="img"
+                        viewBox="0 0 24 24"
+                        xmlns="http://www.w3.org/2000/svg"
+                        id="Linkedin--Streamline-Simple-Icons"
+                        height="24"
+                        width="24"
+                    >
+                        <title>LinkedIn</title>
+                        <path
+                            d="M20.447 20.452h-3.554v-5.569c0 -1.328 -0.027 -3.037 -1.852 -3.037 -1.853 0 -2.136 1.445 -2.136 2.939v5.667H9.351V9h3.414v1.561h0.046c0.477 -0.9 1.637 -1.85 3.37 -1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433c-1.144 0 -2.063 -0.926 -2.063 -2.065 0 -1.138 0.92 -2.063 2.063 -2.063 1.14 0 2.064 0.925 2.064 2.063 0 1.139 -0.925 2.065 -2.064 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C0.792 0 0 0.774 0 1.729v20.542C0 23.227 0.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 0.774 23.2 0 22.222 0h0.003z"
+                            fill="#000000"
+                            stroke-width="1"
+                        ></path>
+                    </svg>
+                </a>
+
+                <a href="#/" target="_blank" aria-label="Stuur ons een mail.">
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="24"
+                        height="24"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        stroke="currentColor"
+                        stroke-width="2"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        class="icon icon-tabler icons-tabler-outline icon-tabler-mail"
+                    >
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                        <path
+                            d="M3 7a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v10a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2v-10z"
+                        />
+                        <path d="M3 7l9 6l9 -6" />
+                    </svg>
+                </a>
             </div>
         </div>
 
