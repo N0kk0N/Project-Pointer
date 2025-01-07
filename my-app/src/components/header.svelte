a26db34b40d51800809482e3571c22413cc29b0d
N0kk0N <jessekaptein18@gmail.com>
2025-01-07 15:54:31 +0100

    Refactor header component layout and enhance experience sharing link with new styles and icons

--- my-app/src/components/header.svelte
+++ my-app/src/components/header.svelte
@@ -1,5 +1,5 @@
-<div class="flex items-center bg-blue-500">
-    <div>
+<div class="flex items-center bg-blue-500 h-20">
+    <div class="flex items-center">
         <a href="#/">
             <img
                 src="/images/pointerLogo.png"
@@ -9,7 +9,7 @@
         </a>
     </div>
 
-    <div class="flex">
+    <div class="flex items-center">
         <a href="#/">
             <img
                 src="/images/kroLogo.png"
@@ -18,8 +18,54 @@
             />
         </a>
 
-        <a href="#/" class="flex">Deel jouw ervaring
-            <img src="/images/right-arrow.png" alt="" class="w-24 h-auto">
+        <a
+            href="#/"
+            class="flex items-center bg-[#DEFF9C] rounded-full pt-2 pb-2 pr-4 pl-4 hover:bg-black hover:text-white transition-all duration-200"
+        >
+            Deel jouw ervaring
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                class="w-4 h-auto ml-2"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+            >
+                <path
+                    d="M10 18l6-6-6-6"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                />
+                <path
+                    d="M16 12H3"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                />
+            </svg>
         </a>
+
+        <a href="#/">
+            <img
+            src="/images/npoLogo.png"
+            alt="NPO Logo"
+            class="w-8 h-auto"
+            />
+        </a>
+
+        <a href="#/">
+            <img
+                src="/images/search.png"
+                alt="Zoek knop"
+                class="w-6 h-auto"
+            />
+        </a>
+
+        <button class="flex items-center justify-center p-2 bg-transparent border-none" aria-label="Hamburgermenu">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="w-8 h-auto" fill="none" stroke="currentColor" stroke-width="4">
+                <rect x="3" y="5" width="18" height="2" stroke="none" fill="currentColor"/>
+                <rect x="3" y="11" width="18" height="2" stroke="none" fill="currentColor"/>
+                <rect x="3" y="17" width="18" height="2" stroke="none" fill="currentColor"/>
+            </svg>
+        </button>
     </div>
 </div>
