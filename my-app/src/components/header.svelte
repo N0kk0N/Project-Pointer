ba0821539d293274e8a12a3d5622a9ff44c02435
N0kk0N <jessekaptein18@gmail.com>
2025-01-07 15:02:19 +0100

    Add header component and integrate it into the main page

---
+++ my-app/src/components/header.svelte
@@ -0,0 +1,21 @@
+<div class="flex items-center bg-blue-500">
+    <div>
+        <a href="#/">
+            <img
+                src="/images/pointerLogo.png"
+                alt="Pointer logo"
+                class="w-40 h-auto"
+            />
+        </a>
+    </div>
+
+    <div>
+        <a href="#/">
+            <img
+                src="/images/kroLogo.png"
+                alt="KRO NCRV Logo"
+                class="w-24 h-auto"
+            />
+        </a>
+    </div>
+</div>
