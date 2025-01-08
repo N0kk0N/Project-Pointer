4fa55523014f82523c9c44e287e1e571b0ef566f
N0kk0N <jessekaptein18@gmail.com>
2025-01-08 15:20:47 +0100

    Update related links to include hash routing for improved navigation

--- my-app/src/components/related.svelte
+++ my-app/src/components/related.svelte
@@ -2,17 +2,17 @@
     <div class="max-w-relatedMax width-full">
         <h2 class="text-6xl mb-6">Ook interessant</h2>
         <div class="grid grid-cols-1 md:grid-cols-3 sm:grid-cols-2 gap-4">
-            <a class="block row-span-1" href="#">
+            <a class="block row-span-1" href="#/">
                 <img class="h-64 object-cover w-full" src="/images/related.png" alt="">
                 <p class="text-xs mt-2">03 maart 2024 | Artikel</p>
                 <p class="text-xl mt-3">Gezondheidsklachten door nachtwerk: 85 procent bedrijfsartsen ziet het</p>
             </a>
-            <a class="block row-span-1" href="#">
+            <a class="block row-span-1" href="#/">
                 <img class="h-64 object-cover w-full" src="/images/related.png" alt="">
                 <p class="text-xs mt-2">03 maart 2024 | Artikel</p>
                 <p class="text-xl mt-3">Gezondheidsklachten door nachtwerk: 85 procent bedrijfsartsen ziet het</p>
             </a>
-            <a class="block row-span-1" href="#">
+            <a class="block row-span-1" href="#/">
                 <img class="h-64 object-cover w-full" src="/images/related.png" alt="">
                 <p class="text-xs mt-2">03 maart 2024 | Artikel</p>
                 <p class="text-xl mt-3">Gezondheidsklachten door nachtwerk: 85 procent bedrijfsartsen ziet het</p>
