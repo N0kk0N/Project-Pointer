c5fd31a061816db8ff51b6e39de9547746321a44
N0kk0N <jessekaptein18@gmail.com>
2025-01-22 19:48:14 +0100

    Enhance intro section with additional explanations and improved link styling for better user experience

--- my-app/src/components/intro.svelte
+++ my-app/src/components/intro.svelte
@@ -25,14 +25,36 @@
         <p class="text-xl leading-9">
             In de interactieve ervaring hieronder wordt de schadekosten van 860
             bedrijven in Nederland in kaart gebracht. In een <a
-            href="https://pointer.kro-ncrv.nl/ongezonde-lucht-kost-nederland-18-miljard-landbouw-grootste-boosdoener"
+                href="https://pointer.kro-ncrv.nl/ongezonde-lucht-kost-nederland-18-miljard-landbouw-grootste-boosdoener"
+                target="_blank"
+                class="underline decoration-black hover:text-[#4D00FF] hover:decoration-[#4D00FF] transition duration-200"
+                >eerder artikel leggen we uit hoe deze schadekosten tot stand is
+                gekomen</a
+            >.
+        </p>
+        <br/><br>
+        <p class="mb-2 font-semibold text-2xl leading-9">Wat is kg CO₂-eq?</p>
+        <p class="text-xl leading-9">
+            In deze interactive hebben we het soms over kg CO₂-eq, maar wat is
+            het eigenlijk?
+            <br><br>
+            kg CO₂-eq staat voor kilogram <a
+            href="https://www.climatepartner.com/nl/kennisbank/woordenlijst/co2-equivalent"
             target="_blank"
             class="underline decoration-black hover:text-[#4D00FF] hover:decoration-[#4D00FF] transition duration-200"
-            >eerder artikel leggen we uit hoe deze schadekosten tot stand is gekomen</a
-        >.
+            >CO₂-equivalent</a
+        >. Het is een eenheid die
+            wordt gebruikt om de impact van verschillende broeikasgassen op het
+            klimaat uit te drukken in termen van hun effect, vergeleken met
+            koolstofdioxide (CO₂). Omdat sommige gassen (zoals methaan en
+            lachgas) veel sterker zijn dan CO₂ in het vasthouden van warmte,
+            worden ze omgerekend naar een equivalent in CO₂. Zo kun je
+            bijvoorbeeld zeggen dat 1 kg methaan een vergelijkbare impact heeft
+            als ongeveer 25 kg CO₂.
+            <br><br>
+            Het helpt om de klimaatimpact van
+            verschillende activiteiten of producten te vergelijken, bijvoorbeeld
+            van voedsel, vervoer of energiegebruik.
         </p>
     </div>
 </div>
-
-
-<!-- https://www.climatepartner.com/nl/kennisbank/woordenlijst/co2-equivalent -->
